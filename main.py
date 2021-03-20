1f150eb65d6ca8bab393b5bcd8643d45548081c9
Hayden McAlister <haydenmcalister49@gmail.com>
2021-03-21 10:12:33 +1300

    Updated methods with new ideas. Starting black square thread now requires the initial value as well as the count. Correction method now uses constant movements and timers to find angle correction for smoother operation

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 #!/user/bin/env python3
-from ev3dev2. motor import LargeMotor, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
+from ev3dev2. motor import LargeMotor, OUTPUT_B, OUTPUT_C, SpeedPercent, SpeedDPS, MoveTank
 from ev3dev2.sensor.lego import TouchSensor, UltrasonicSensor, ColorSensor
 from ev3dev2.sound import Sound
 from ev3dev2.display import Display
-from textwrap import wrap
 import threading
 import math
 import time
@@ -16,8 +15,7 @@ board_dimensions = [14, 7]
 
 
 class BlackSquareSensor:
-    ROLLING_AVERAGE_COUNT = 5
-    VALUE_LIST = [0]*ROLLING_AVERAGE_COUNT
+    VALUE_LIST = None
     VALUE_LIST_LOCK = threading.Lock()
     CONSTANT_READ = False
     CURRENT_INDEX = 0
@@ -35,33 +33,12 @@ class BlackSquareSensor:
 
         #Set the sensor and take an initial value
         self.SENSOR = ColorSensor(sensor.address)
-        value = self.take_reading()
-        #Set the amount of values to average over
-        self.ROLLING_AVERAGE_COUNT = count
-        #Define the start of the list to be only the first reading (simulate sitting at start for a while)
-        self.VALUE_LIST_LOCK.acquire()
-        self.VALUE_LIST = [value]*self.ROLLING_AVERAGE_COUNT
-        self.VALUE_LIST_LOCK.release()
 
-    def take_reading(self):
-        """
-        Take a reading from the color sensor
-        Extracted to a method so this can be easily changed later
-        :return: The reading of the color sensor
-        """
-
-        #TODO find a good way to take a reading
-        #Make sure we only take a reading when the lock is in our hands
-        self.VALUE_LIST_LOCK.acquire()
-        value = self.SENSOR.reflected_light_intensity
-        self.VALUE_LIST[self.CURRENT_INDEX] = value
-        self.CURRENT_INDEX = (self.CURRENT_INDEX+1) % len(self.VALUE_LIST)
-        self.VALUE_LIST_LOCK.release()
-        return value
-
-    def start_reading(self, interval, wait_time):
+    def start_reading(self, count, init_val, interval, wait_time):
         """
         Start taking constant readings on a thread
+        :param count: The length of the value array, the number of values to average over
+        :param init_val: The value to initalise the new array to, between 0 and 100
         :param interval: The interval to take readings at
         :param wait_time: A set amount of time to wait before we start reading
         :return: None
@@ -72,10 +49,10 @@ class BlackSquareSensor:
             self.stop_reading()
 
         self.VALUE_LIST_LOCK.acquire()
-        value = self.take_reading()
-        self.VALUE_LIST = [value] * self.ROLLING_AVERAGE_COUNT
+        self.VALUE_LIST = [init_val] * count
         self.VALUE_LIST_LOCK.release()
 
+        self.CURRENT_INDEX = 0
         self.CONSTANT_READ = True
         self.THREAD = threading.Thread(target=self.constant_read, args=(self, interval, wait_time, ))
         self.THREAD.start()
@@ -111,6 +88,22 @@ class BlackSquareSensor:
         while self.CONSTANT_READ:
             time.sleep(interval)
             self.take_reading()
+
+    def take_reading(self):
+        """
+        Take a reading from the color sensor
+        Extracted to a method so this can be easily changed later
+        :return: The reading of the color sensor
+        """
+
+        #TODO find a good way to take a reading
+        #Make sure we only take a reading when the lock is in our hands
+        self.VALUE_LIST_LOCK.acquire()
+        value = self.SENSOR.reflected_light_intensity
+        self.VALUE_LIST[self.CURRENT_INDEX] = value
+        self.CURRENT_INDEX = (self.CURRENT_INDEX+1) % len(self.VALUE_LIST)
+        self.VALUE_LIST_LOCK.release()
+        return value
 
     def get_average_result(self):
         """
@@ -159,7 +152,7 @@ class Robot:
         self.position = start_position
         self.direction = start_direction
 
-    def move(self, speed=SpeedPercent(50)):
+    def move(self, speed=50):
         """
         Move forward as a tank until it hits a black square and update the position
         :param speed: The speed to move
@@ -170,10 +163,11 @@ class Robot:
         self.position[0] += self.direction[0]
         self.position[1] += self.direction[1]
 
-        self.black_square_sensor.start_reading(0.25, 1)
+        #We start on a black square so we initalise to being on a white square
+        self.black_square_sensor.start_reading(count=3, init_val=100, interval=0.25, wait_time=1)
 
         #Until we hit a black square, just keep moving forward
-        self.tank.on(speed,speed)
+        self.tank.on(SpeedPercent(speed),SpeedPercent(speed))
         #Block until we are over a black square
         while self.black_square_sensor.above_threshold():
             continue
@@ -182,7 +176,7 @@ class Robot:
         self.report_black_square()
         self.correction()
 
-    def check_next(self, speed=SpeedPercent(30)):
+    def check_next(self, speed=30):
         """
         Perform a check of the space between current position and next black square
         This is very similiar to the move function, but has checks for touch
@@ -194,15 +188,16 @@ class Robot:
         self.position[0] += 0.5*self.direction[0]
         self.position[1] += 0.5*self.direction[1]
 
-        self.black_square_sensor.start_reading(0.25, 1)
+        #We are on a black square, so we initalise to white
+        self.black_square_sensor.start_reading(count=3, init_val=100, interval=0.25, wait_time=1)
 
         #TODO ensure that this finds the tower well and doesn't push it
         # Could use the sonar sensor too
 
         # Until we hit a black square, just keep moving forward
-        self.tank.on(speed, speed)
-        # Block until we are over a black square
-        while self.black_square_sensor.above_threshold() or not self.touch_sensor.is_pressed:
+        self.tank.on(SpeedPercent(speed), SpeedPercent(speed))
+        # Block until we are over a black square or until we touch something
+        while self.black_square_sensor.above_threshold() and not self.touch_sensor.is_pressed:
             continue
         self.tank.off()
         if self.touch_sensor.is_pressed:
@@ -214,7 +209,7 @@ class Robot:
         self.correction()
         return False
 
-    def correction(self):
+    def correction(self, dps=30):
         """
         Do a correction for any deviation after travelling between squares
         The implementation below assumes that we didn't deviate too far between black squares
@@ -224,37 +219,44 @@ class Robot:
         The implementation is to find the angle left and right until we are off the square,
         take the midpoint and correct this amount.
         This should put us on a roughly equal but opposite deviation compared to the last trip, canceling the next problem
+        :param dps: The speed to turn in degrees per second
         :return: None
         """
 
         # Stop the old thread to start a new one with better parameters
         self.black_square_sensor.stop_reading()
-        self.black_square_sensor.start_reading(0.1, 0)
-
-        angle_step = 2
-        speed = SpeedPercent(30)
-        left_angle = 0
-        right_angle = 0
+        #Currently on a black square, move until on a white square
+        self.black_square_sensor.start_reading(count=2, init_val=0, interval=0.1, wait_time=0)
 
         #TODO Ensure this makes an adeuqete correction between both square spacings
         #While we are not back on the white keep turning
+        start = time.time()
+        self.tank.on(SpeedDPS(-dps), SpeedDPS(dps))
         while not self.black_square_sensor.above_threshold():
-            self.tank.on_for_degrees(-speed, speed, angle_step)
-            left_angle += angle_step
+            continue
+        end = time.time()
+        left_angle = dps*(end-start)
         #We now know angular deviation to left, reset by moving back
-        self.tank.on_for_degrees(speed, -speed, left_angle)
+        self.tank.on_for_degrees(SpeedDPS(dps), SpeedDPS(-dps), left_angle)
+
         #Do the same for the right angle
+        self.black_square_sensor.start_reading(count=2, init_val=0, interval=0.1, wait_time=0)
+        start = time.time()
+        self.tank.on(SpeedDPS(-dps), SpeedDPS(dps))
         while not self.black_square_sensor.above_threshold():
-            self.tank.on_for_degrees(speed, -speed, angle_step)
-            right_angle += angle_step
-        #Now we have both left and right angles, lets average then move to corrected bearing
-        angle_correction = (left_angle-right_angle)/2
-        self.tank.on_for_degrees(speed, -speed, right_angle+angle_correction)
+            continue
+        end = time.time()
+        right_angle = dps * (end - start)
+        # We now know angular deviation to left, reset by moving back
+        self.tank.on_for_degrees(SpeedDPS(dps), SpeedDPS(-dps), left_angle)
 
+        # Now we have both left and right angles, lets average then move to corrected bearing
+        angle_correction = (left_angle - right_angle) / 2
+        self.tank.on_for_degrees(SpeedDPS(dps), 0, right_angle + angle_correction)
 
-    def rotate(self, angle, speed=SpeedPercent(30)):
+    def rotate(self, angle_count, speed=30):
         """
-        Rotate an angle (in degrees) on an axis like a tank and update the direction vector
+        Rotate a multiple of 90 degrees on axis like a tank and update the direction vector
         We are defining positive rotation as turning anti-clockwise
         :param angle: the angle to rotate through
         :param speed: The speed to rotate around at
@@ -263,20 +265,19 @@ class Robot:
 
         # Update direction using rotation matrix
         #TODO ensure this correctly alters direction vector
-        old_direction = self.direction
-        angle_rads = 180 * angle / math.pi
-        self.direction[0] = old_direction[0] * math.cos(angle_rads) - old_direction[1] * math.sin(angle_rads)
-        self.direction[1] = old_direction[0] * math.sin(angle_rads) + old_direction[1] * math.cos(angle_rads)
+        direction_matrix = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        current_index = direction_matrix.index(self.direction)
+        self.direction = direction_matrix[(current_index + angle_count) % len(direction_matrix)]
 
         #TODO Ensure this correctly rotates robot
         #First, prepare by moving a distance so we are over the black square
-        self.tank.on_for_rotations(speed, speed, self.DISTANCE_TO_ROTATION_AXIS)
+        self.tank.on_for_rotations(SpeedPercent(speed), SpeedPercent(speed), self.DISTANCE_TO_ROTATION_AXIS)
 
         #Now actually rotate on the axis
-        self.tank.on_for_degrees(-speed, speed, angle)
+        self.tank.on_for_degrees(SpeedPercent(-speed), SpeedPercent(speed), 1.9*angle_count*90)
 
         #Finally, undo the prepartation by moving backwards and placing the light sensor over the black square
-        self.tank.on_for_rotations(speed, speed, -self.DISTANCE_TO_ROTATION_AXIS)
+        self.tank.on_for_rotations(SpeedPercent(speed), SpeedPercent(speed), -self.DISTANCE_TO_ROTATION_AXIS)
 
 
     def report_black_square(self):
@@ -300,7 +301,7 @@ class Robot:
         self.display_text(blue_number)
         self.sound.beep()
 
-    def display_text(self, string, font_name='courB24', font_width=15, font_height=24):
+    def display_text(self, string, font_name='courB24'):
         """
         Display some text on the lcd Display
         :param string: The string to display
@@ -311,10 +312,6 @@ class Robot:
         """
 
         self.lcd.clear()
-        strings = wrap(string, width=int(180 / font_width))
-        for i in range(len(strings)):
-            x_val = 89 - font_width / 2 * len(strings[i])
-            y_val = 63 - (font_height + 1) * (len(strings) / 2 - i)
-            self.lcd.text_pixels(strings[i], False, x_val, y_val, font=font_name)
+        self.lcd.text_pixels(string, clear_screen=True, x=30, y=30, font=font_name)
         self.lcd.update()
 
