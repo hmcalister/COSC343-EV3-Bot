d1569189a5550c20902400bca3ab885c57dfccbd
Gamma749 <39180428+Gamma749@users.noreply.github.com>
2021-03-29 18:54:08 +1300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def correction(angle_correction):
-    angle_correction = 0.75*(-90 / math.pi) * math.atan(math.radians(angle_correction))
-    return angle_correction
-
-for i in range(-25, 25):
-    print(f'{i}: {-i/3:.2f} {correction(i)}')
